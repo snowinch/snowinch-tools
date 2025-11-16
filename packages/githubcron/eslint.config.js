4c46abae228661e931ae8f12101427e658ff3658
Manuel Tardivo <manuel.tardivo@snowinch.com>
2025-11-16 13:36:41 +0000

    fix(ci): add eslint config for githubcron package

---
+++ packages/githubcron/eslint.config.js
@@ -0,0 +1,13 @@
+import { config } from "@repo/eslint-config/base.js";
+
+/**
+ * ESLint configuration for @snowinch/githubcron package
+ *
+ * @type {import("eslint").Linter.Config[]}
+ */
+export default [
+  ...config,
+  {
+    ignores: ["dist/**", "coverage/**", "node_modules/**"],
+  },
+];
